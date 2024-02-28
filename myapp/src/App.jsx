a0765a3c58be408a30523fb92abc3e20e95cc6fe
manojgarnayak <manojgarnayak07@gmail.com>
2024-02-28 20:13:20 +0530

    First Files

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+import MyHeading from "./components/MyHeading";
+const App = () => {
+  return (
+    <>
+      <div>
+        <MyHeading/>
+        <h2>Welcome to React Page</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/MyHeading.jsx
@@ -0,0 +1,13 @@
+
+const MyHeading = () => {
+    const bhalu = "Manoj"
+  return (
+    <>
+        <div>
+            <h1>This is the Heading {bhalu}</h1>
+        </div>
+    </>
+  )
+}
+
+export default MyHeading;
