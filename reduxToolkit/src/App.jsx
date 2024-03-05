e0818febc4d033edd44526f007d27237e0b91bc2
manojgarnayak <manojgarnayak07@gmail.com>
2024-03-05 12:36:59 +0530

    Files

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import AddToDo from './components/AddToDo'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learning about Redux Toolkit</h1>
+      <AddToDo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
